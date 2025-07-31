40ca52f4b7913d9e7aa3d6b81cf42639c0b186e2
Andrey <kotlyar.andrey03@mail.ru>
2025-08-01 00:39:49 +0300

    Добавли меню бургер и табы, сайт будет разделен не отдельными страницами а контентом табов

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,4 @@
+//Смена темы
 const toggleBtn: HTMLElement | null = document.getElementById("theme-toggle");
 
 if (toggleBtn) {
@@ -5,3 +6,63 @@ if (toggleBtn) {
     document.documentElement.classList.toggle("dark__theme");
 });
 }
+
+
+//Меню бургер
+const navigation = document.querySelector<HTMLElement>('.navigation');
+const openNavigation = document.querySelector<HTMLElement>('.open__navigation');
+const navigationActive = document.querySelector<HTMLElement>('.navigation__active');
+
+openNavigation?.addEventListener('click', () => {
+  if (navigationActive) {
+    navigationActive.classList.toggle('navigation__active-open');
+  }
+
+   openNavigation.classList.toggle("is-open");
+   navigation?.classList.toggle('navigation__open');
+});
+
+
+
+//Табы (меню навигации)
+const tabs = document.querySelectorAll<HTMLElement>('.tabheader__item'),
+      tabsContent = document.querySelectorAll<HTMLElement>('.tabcontent'),
+      tabsParent = document.querySelector<HTMLElement>('.navigation');
+      //Вызываю здесь закрытие модалки потому что табы скрываются в бургер на экранах меньше 950 пикселей
+      tabs.forEach((item) => {
+        item.addEventListener('click', () => {
+          navigation?.classList.remove('navigation__open');
+          navigationActive?.classList.remove('navigation__active-open');
+          openNavigation?.classList.toggle("is-open");
+        })
+      })
+
+function hideTab() {
+    tabs.forEach((item) => {
+        item.classList.remove('tabheader__item-active');
+    });
+    
+    tabsContent.forEach((item) => {
+        item.style.display = 'none';
+    });
+}
+hideTab();
+
+function showTab(i = 0) {
+      tabs[i].classList.add('tabheader__item-active');
+      tabsContent[i].style.display = 'block';
+}
+showTab();
+
+tabsParent?.addEventListener('click', (e) => {
+    const target = e.target as HTMLElement;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+          if (target == item) {
+              hideTab();
+              showTab(i);
+          }
+      });
+    }
+});
